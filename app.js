8302c204a27f832453cca65bb5e8b73862c8961f
Abin <abinyadhav@gmail.com>
2026-03-15 20:32:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req,res)=>{
- res.send("Full Stack Deployment using Jenkins")
+ res.send("Full Stack Deployment using Jenkinss")
 })
 
 app.listen(3000,"0.0.0.0",()=>{
