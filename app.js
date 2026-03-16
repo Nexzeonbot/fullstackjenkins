ca2c7ffb0f732398bffab2942995a72192c7253f
Abin <abinyadhav@gmail.com>
2026-03-16 09:44:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-const express = require("express")
-const app = express()
-
-app.get("/", (req,res)=>{
- res.send("Full Stack Deployment using Jenkinss")
-})
-
-app.listen(3000,"0.0.0.0",()=>{
- console.log("Server running on port 3000")
-})
+res.send("CI/CD Pipeline Working 🚀")
